38bf20161622803e428eb5f610dbc41862774d8f
Caelan O'Sullivan <caelanosullivan@gmail.com>
2018-01-09 17:00:54 -0600

    Updated top level welcome routes documentation to clarify URLs

--- app.py
+++ app.py
@@ -45,8 +45,8 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<'start'>    Note: append start date as YYYY-MM-DD <br/>"
-        f"/api/v1.0/<start>/<end>"
+        f"/api/v1.0/<'start'>    Note: append dates as YYYY-MM-DD <br/>"
+        f"/api/v1.0/<'start'>/<'end'>    Note: append dates as YYYY-MM-DD"
     )
 
 # hello_dict = {"Hello": "World!"}
